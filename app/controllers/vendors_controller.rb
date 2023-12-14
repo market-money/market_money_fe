7d2f5c2657a3cead6b0d4dcfb257955e78ce94f7
Sooyung Kim <kimx1279@gmail.com>
2023-12-14 10:14:19 -0700

    Add poro test

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,5 +1,8 @@
 class VendorsController < ApplicationController
   def show
-    
+    conn = Faraday.new(url: 'http://localhost:3000')
+    vendor_response = conn.get("/api/v0/vendors/#{params[:id]}")
+    vendor_json = JSON.parse(vendor_response.body, symbolize_names: true)[:data]
+    @vendor = Vendor.new(vendor_json)
   end
 end
